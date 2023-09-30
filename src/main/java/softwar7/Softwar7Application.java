d8b5fb9514cdf46da52cd399891e28141b5aa472
Jung-YouHwan <yhwjd99@gmail.com>
2023-09-30 21:26:16 +0900

    초기 설정

---
+++ src/main/java/softwar7/Softwar7Application.java
@@ -0,0 +1,13 @@
+package softwar7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Softwar7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Softwar7Application.class, args);
+    }
+
+}
---
+++ src/test/java/softwar7/Softwar7ApplicationTests.java
@@ -0,0 +1,13 @@
+package softwar7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Softwar7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
