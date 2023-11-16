9ece3a3dea8f8123a4506605bfd4cf6741029613
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-16 19:58:06 +0900

    fix : url 오류 수정

--- src/main/java/softwar7/presentation/schedule/ScheduleController.java
+++ src/main/java/softwar7/presentation/schedule/ScheduleController.java
@@ -50,25 +50,29 @@ public class ScheduleController {
     }
 
     @GetMapping("/guest/schedules")
-    public ScheduleResult getSchedules(@Login final MemberSession memberSession,
-                                       @RequestParam(required = false)
-                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime start,
-                                       @RequestParam(required = false)
-                                       @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime end,
-                                       @RequestParam(required = false) final long channelId
+    public ScheduleResult getSchedules(
+            @Login final MemberSession memberSession,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime start,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime end,
+            @RequestParam(required = false) final long channelId
     ) {
+
         List<ScheduleResponse> scheduleResponses = scheduleFindService.findAllSchedulesByDate(start, end, channelId);
         return new ScheduleResult(scheduleResponses);
     }
 
-    @GetMapping("/guest/schedules/request")
-    public List<ScheduleResponse> getRequestSchedules(@Login final MemberSession memberSession,
-                      @RequestParam(required = false) final Approval approval,
-                      @RequestParam(required = false)
-                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime start,
-                      @RequestParam(required = false)
-                      @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime end) {
-        return scheduleFindService.findAllMemberSchedules(start, end, approval,memberSession.id());
+    @GetMapping("/guest/schedules/requests")
+    public List<ScheduleResponse> getRequestSchedules(
+            @Login final MemberSession memberSession,
+            @RequestParam(required = false) final Approval approval,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime start,
+            @RequestParam(required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) final LocalDateTime end
+    ) {
+        return scheduleFindService.findAllMemberSchedules(start, end, approval, memberSession.id());
     }
 
     @GetMapping("/admin/schedules/requests")
