deeaa3de428a942aff2d5c5bdcab5929660c288d
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-16 19:40:45 +0900

    fix : 스케줄 생성시 채널 검증

--- src/main/java/softwar7/application/schedule/ScheduleCreateService.java
+++ src/main/java/softwar7/application/schedule/ScheduleCreateService.java
@@ -6,19 +6,24 @@ import softwar7.domain.member.vo.MemberSession;
 import softwar7.domain.schedule.persist.Schedule;
 import softwar7.mapper.shedule.ScheduleMapper;
 import softwar7.mapper.shedule.dto.ScheduleSaveRequest;
+import softwar7.repository.channel.ChannelRepository;
 import softwar7.repository.schedule.ScheduleRepository;
 
 @Service
 public class ScheduleCreateService {
 
+    private final ChannelRepository channelRepository;
     private final ScheduleRepository scheduleRepository;
 
-    public ScheduleCreateService(final ScheduleRepository scheduleRepository) {
+    public ScheduleCreateService(final ChannelRepository channelRepository,
+                                 final ScheduleRepository scheduleRepository) {
+        this.channelRepository = channelRepository;
         this.scheduleRepository = scheduleRepository;
     }
 
     @Transactional
     public void create(final MemberSession memberSession, final ScheduleSaveRequest dto) {
+        channelRepository.getById(dto.channelId());
         Schedule schedule = ScheduleMapper.toEntity(memberSession, dto);
         scheduleRepository.save(schedule);
     }
