30db5a2f462db280abfab03cb16c149981b9a304
Jung-YouHwan <yhwjd99@gmail.com>
2023-11-21 09:54:17 +0900

    refactor : 서블릿 에러 오류 잡기 추가

--- src/main/java/softwar7/global/exception/ApiRestControllerAdvice.java
+++ src/main/java/softwar7/global/exception/ApiRestControllerAdvice.java
@@ -2,10 +2,15 @@ package softwar7.global.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import softwar7.global.exception.dto.ExceptionResponse;
+
+import static org.springframework.web.client.HttpClientErrorException.*;
 
 
 @RestControllerAdvice
@@ -14,10 +19,26 @@ public class ApiRestControllerAdvice {
     // 400
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ExceptionResponse methodArgumentNotValidException(
+    public ExceptionResponse handleException(
             final MethodArgumentNotValidException e
     ) {
         return new ExceptionResponse("400", "형식에 맞지 않습니다");
+    }
+
+    // 400
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ExceptionResponse handleException(
+            final MethodArgumentTypeMismatchException e
+    ) {
+        return new ExceptionResponse("400", "서버에 전달한 인자와 타입이 맞지 않습니다");
+    }
+
+    // 400
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ServletRequestBindingException.class)
+    public ExceptionResponse handleException(final ServletRequestBindingException e) {
+        return new ExceptionResponse("400", "파라미터나 HTTP 바디 데이터를 바인딩 할 수 없습니다");
     }
 
     // 400
@@ -46,5 +67,12 @@ public class ApiRestControllerAdvice {
     @ExceptionHandler(NotFoundException.class)
     public ExceptionResponse handleException(final NotFoundException e) {
         return new ExceptionResponse(e.getStatusCode(), e.getMessage());
+    }
+
+    // 405
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(MethodNotAllowed.class)
+    public ExceptionResponse handleException(final MethodNotAllowed e) {
+        return new ExceptionResponse("405", "HTTP 메소드가 올바르지 않습니다");
     }
 }
