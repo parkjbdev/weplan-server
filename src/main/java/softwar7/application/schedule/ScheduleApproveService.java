9f9088551f5871e18abeaa000222835e78aa4916
Jung-YouHwan <yhwjd99@gmail.com>
2023-12-05 20:24:59 +0900

    fix : 스케줄 승인 거절 오류 수정

--- src/main/java/softwar7/application/schedule/ScheduleApproveService.java
+++ src/main/java/softwar7/application/schedule/ScheduleApproveService.java
@@ -3,11 +3,13 @@ package softwar7.application.schedule;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import softwar7.domain.schedule.persist.Schedule;
+import softwar7.domain.schedule.vo.Approval;
 import softwar7.global.constant.ExceptionMessage;
 import softwar7.global.exception.BadRequestException;
 import softwar7.mapper.shedule.dto.ScheduleApproveRequest;
 import softwar7.repository.schedule.ScheduleRepository;
 
+import static softwar7.domain.schedule.vo.Approval.*;
 import static softwar7.global.constant.ExceptionMessage.*;
 
 @Service
@@ -26,7 +28,7 @@ public class ScheduleApproveService {
                 scheduleRepository.isNotOverlapping(schedule.getChannelId(),
                         schedule.getStartTime(), schedule.getEndTime());
 
-        if (!isNotOverlapping) {
+        if (!isNotOverlapping && dto.approval().equals(APPROVED)) {
             throw new BadRequestException(ALREADY_EXIST_SCHEDULE.message);
         }
 
